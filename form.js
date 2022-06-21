80df10a82df0c03eb262e4fe79eec8f0e513ff8f
LeonardoAraneda <leonardoaranedam@gmail.com>
2022-06-21 17:27:28 -0400

    Test Conection

--- form.js
+++ form.js
@@ -4,8 +4,24 @@ form.addEventListener("submit", function(event){
     event.preventDefault();
     let formdata = new FormData(form);
     let formobj = convertirformdataaobjeto(formdata)
-
+    insertartabla (formobj)
+    guardarobj(formobj)
+    
 })
+
+function insertartabla (formobj) {
+    let tablaref = document.getElementById("visortabla");
+    let nuevafila = tablaref.insertRow(-1);
+    let nuevotipocelda = nuevafila.insertCell(0);
+    nuevotipocelda.textContent = formobj["nombre"];
+
+    nuevotipocelda = nuevafila.insertCell(1);
+    nuevotipocelda.textContent = formobj["rut"];
+
+    nuevotipocelda = nuevafila.insertCell(2);
+    nuevotipocelda.textContent = formobj["cantidad"];
+
+}
 
 function convertirformdataaobjeto(formdata){
     let formnombre= formdata.get("nombre")
@@ -17,9 +33,21 @@ function convertirformdataaobjeto(formdata){
         "cantidad": formcantidad
     }
 }
-
+ 
 function guardarobj(formobj){
-    let formobjJSON = JSON.stringify(formobj)
-    localStorage.setItem("data", formobjJSON)
+    let formarray = JSON.parse(localStorage.getItem("data")) || [];
+    formarray.push(formobj)
+    //Convierto mi array en JSON
+    let formoarrayJSON = JSON.stringify(formarray)
+    //Guardo el array en formato JSON en el local storage
+    localStorage.setItem("data", formoarrayJSON)
+
+    fetch ("https://frozen-scrubland-95507.herokuapp.com/users/", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: formoarrayJSON,
+    });
 }
 
